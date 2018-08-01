ed5fa1467631ca91c81849e0c509418c2bd4db86
Ricky Liu <rorix12321@gmail.com>
2018-08-01 00:57:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,8 @@ function removeFromCart(itemName) {
     for (let i = 0; i < cart.length; i++) {
     let removed = cart[i];
     if (removed.itemName === itemName) {
-      cart.splice(removed);
+      cart.splice(removed, 1);
       return cart;
     } else {
       return "That item is not in your cart.";
-    }
 }
