1c5c8a9d33ca71798d4210dbe5e0d143c5221606
Ricky Liu <rorix12321@gmail.com>
2018-08-01 00:25:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function addToCart(item) {
-    return cart;
+var cart = [];
+
+function getCart() {
+ return cart;
+}
+
+function setCart(c) {
+  cart = c;
+  return cart;
+}
+
+function addToCart(itemName) {
+  let item = {itemName: itemName,
+    itemPrice: Math.floor(Math.random()* 100 + 1)};
+    cart.push(itemName);
+    return `${itemName} has been added to your cart.`
 }
